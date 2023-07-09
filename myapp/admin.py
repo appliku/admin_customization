3e1c7c196833018aa1e342041ddf0a4a728e681d
Kostja P <kpavlovsky@users.noreply.github.com>
2023-07-09 10:19:04 +0200

    prepopulated_fields

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Category, Product, Customer, Order
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'slug', 'is_active', 'id',)
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'slug', 'is_active', 'id',)
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'phone', 'id',)
+
+
+admin.site.register(Customer, CustomerAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'created_dt', 'completed_dt', 'status', 'id',)
+
+
+admin.site.register(Order, OrderAdmin)
